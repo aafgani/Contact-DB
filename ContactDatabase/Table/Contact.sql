3e403e09ee89b5fbdc4db313d655ba4d1ed35ead
aafgani <andrya.afgani@gmail.com>
2023-03-10 09:48:27 +0700

    create contact db

---
+++ ContactDatabase/Table/Contact.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Contact]
+(
+	[Id] INT NOT NULL IDENTITY(1,1) PRIMARY KEY, 
+    [FirstName] NVARCHAR(50) NULL, 
+    [SurName] NVARCHAR(50) NULL, 
+    [EmailAddress] NVARCHAR(50) NULL, 
+    [MobileNumber] NVARCHAR(50) NULL, 
+    [CreatedAt] DATETIME NOT NULL
+)
